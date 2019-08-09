5ff486c31b7503306c08b5b8294c45f7415c1fb9
Kevin John <ultima301@gmail.com>
2019-08-08 18:52:50 -0700

    - Imported lots of temp sprites - Working on data structures for map generation

---
+++ Assets/Managers/MapManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour {
+
+	public static MapManager instance;
+
+	[System.Serializable]
+	public class WeightedMapTile : LSWeightedItem<GameObject> { public WeightedMapTile() : base() { } }
+
+	public WeightedMapTile[] obstacleOptions;
+	LSWeightedList<GameObject> mapTiles;
+
+	void Awake()
+	{
+		instance = this;
+		//mapTiles = new LSWeightedList<GameObject>{obstacleOptions};
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
