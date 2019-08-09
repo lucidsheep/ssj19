47695f6370def44f93bd43217a820af7c7727169
Kevin John <ultima301@gmail.com>
2019-08-09 14:07:13 -0700

    Basic map generation

---
+++ Assets/Components/MapTile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapTile : MonoBehaviour {
+
+	public enum TileCategory { TREE, WATER, THORNS, ROCK, FIRE }
+	public bool isObstacle;
+
+	public float containsFoodProbability;
+	public Food containedFood;
+
+	public bool isDangerous;
+	public IntRange damageRange;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Managers/MapManager.cs
+++ Assets/Managers/MapManager.cs
@@ -7,21 +7,40 @@ public class MapManager : MonoBehaviour {
 	public static MapManager instance;
 
 	[System.Serializable]
-	public class WeightedMapTile : LSWeightedItem<GameObject> { public WeightedMapTile() : base() { } }
+	public class WeightedMapTile : LSWeightedItem<MapTile> { public WeightedMapTile() : base() { } }
 
+	public MapTile groundTile;
 	public WeightedMapTile[] obstacleOptions;
-	LSWeightedList<GameObject> mapTiles;
+
+	public IntRange tileSize;
+
+	LSWeightedList<MapTile> mapTiles;
 
 	void Awake()
 	{
 		instance = this;
-		//mapTiles = new LSWeightedList<GameObject>{obstacleOptions};
+		mapTiles = new LSWeightedList<MapTile>();
+		foreach(var tile in obstacleOptions)
+		{
+			mapTiles.Add(tile.item, tile.weight);
+		}
 	}
 	// Use this for initialization
 	void Start () {
-		
+		GenerateMap(50, 10);
 	}
-	
+
+	void GenerateMap(int size, int difficulty)
+	{
+		Util.DoubleLoop(size, size, (x, y) =>
+		{
+			MapTile baseTile = Instantiate(groundTile, new Vector3(x * tileSize.x, y * tileSize.y, 0f), Quaternion.identity);
+
+			MapTile randomTile = mapTiles.GetRandomItem();
+			if(randomTile != null)
+				Instantiate(randomTile, new Vector3(x * tileSize.x, y * tileSize.y, 0f), Quaternion.identity);
+		});
+	}
 	// Update is called once per frame
 	void Update () {
 		
